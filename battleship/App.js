f337c39529acb76eb248bbbb92aa4bc226238590
Borja <k0esbo00@students.oamk.fi>
2021-02-24 19:38:19 +0200

    ee

--- battleship/App.js
+++ battleship/App.js
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import styles from './style/style'
+import Gameboard from './components/Gameboard';
 
 export default class App extends React.Component {
   constructor(props){
@@ -13,6 +14,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Header />
+        <Gameboard />
         <Footer />
       </View>
     )
